2a60bbd3bf1f15f5233a61648a90cca736b47e74
Olaf Prins <olaf.prins@web.de>
2018-01-26 21:41:29 +0100

    Do not symlink the keyboard layout

--- install.sh
+++ install.sh
@@ -11,6 +11,4 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/macos/keyboard/U.S-daniel.keylayout" ~/Library/Keyboard\ Layouts
-ln -sfv "$DOTFILES_DIR/macos/keyboard/U.S-daniel.icns" ~/Library/Keyboard\ Layouts
 #ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~/bash_profile
