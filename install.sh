341bacd9138bf235b9a4a22969e9a2df08ede2cf
Olaf Prins <olaf.prins@web.de>
2018-01-27 12:57:14 +0100

    Activate dotfiles .bash_profile

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-#ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~/bash_profile
+ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~/.bash_profile
--- system/.bash_profile
+++ system/.bash_profile
@@ -1,4 +1,5 @@
 
+# .bash_profile
 
 # --- Interactivity check
 
