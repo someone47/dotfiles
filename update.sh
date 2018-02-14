6a80bc6edb3e9eaaeea0fcc726010d5064aaf084
full name <email>
2018-02-14 21:42:16 +0100

    Add an update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+# update.sh
+
+# Update App Store apps
+sudo softwareupdate -i -a
+
+# Update Homebrew (Cask) & packages
+brew update
+brew upgrade
+
+# Update npm & packages
+npm install npm -g
+npm update -g
